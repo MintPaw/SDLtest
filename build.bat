dddf2f21c87e501f32e68e62f3acd3ebc853094f
MintPaw <jerusanders@gmail.com>
2015-06-03 15:13:48 -0600

    Added SDL_images

--- build.bat
+++ build.bat
@@ -1,4 +1,14 @@
 SET PATH=%PATH%;C:\MinGW\bin
 del bin\game.exe
 cls
-g++ src\main.cpp -I lib\SDL2-2.0.3\include\ -L lib\SDL2-2.0.3\lib\x86\ -lSDL2main -lSDL2 -lmingw32 -o bin\game.exe -w
+g++ src\main.cpp^
+	-I lib\SDL2-2.0.3\include\^
+	-I lib\SDL_image-1.2.12\include\^
+	-L lib\SDL2-2.0.3\lib\x86\^
+	-L lib\SDL_image-1.2.12\lib\^
+	-lSDL2main^
+	-lSDL2^
+	-lmingw32^
+	-lSDL_image^
+	-o bin\game.exe^
+	-w -g -Wall
