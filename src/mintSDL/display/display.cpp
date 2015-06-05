1b455cc668a54bfc7b359e95e4e510b378053621
MintPaw <jerusanders@gmail.com>
2015-06-05 16:04:59 -0600

    Fixed SDL_Color to Hex

--- src/mintSDL/display/display.cpp
+++ src/mintSDL/display/display.cpp
@@ -32,5 +32,5 @@ SDL_Color* mint_DisplayHexToSDLColor(unsigned long colour)
 
 unsigned long mint_DisplayToSDLColorHex(SDL_Color* sdlColor)
 {   
-    return ((sdlColor->a & 0xff) << 32) + (sdlColor->r & 0xff) << 16) + ((sdlColor->g & 0xff) << 8) + (sdlColor->b & 0xff);
+    return (((sdlColor->a & 0xff) << 32) + ((sdlColor->r & 0xff) << 16) + ((sdlColor->g & 0xff) << 8) + (sdlColor->b & 0xff));
 }
