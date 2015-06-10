d60424c70f1132ff735cd9cb6d7411b0c11fc6be
MintPaw <jerusanders@gmail.com>
2015-06-10 15:53:57 -0600

    Added missing includes for mingw

--- src/mintSDL/maths/phys.cpp
+++ src/mintSDL/maths/phys.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "mintSDL/display/texture.h"
 #include "mintSDL/maths/phys.h"
 
--- src/mintSDL/timer.cpp
+++ src/mintSDL/timer.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <SDL.h>
 #include "mintSDL/timer.h"
