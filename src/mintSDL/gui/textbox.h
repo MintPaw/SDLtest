73158bad332197e53218e7e132befce226be1668
brutalexcess <selman.luke1@gmail.com>
2015-06-13 21:45:07 +0100

    Update textbox.h

--- src/mintSDL/gui/textbox.h
+++ src/mintSDL/gui/textbox.h
@@ -22,14 +22,14 @@ struct MintBlinkingCaret {
 	int blinkSpeed;
 };
 
-TextBox mint_CreateTextBox(int x, int y, int width, int height); //Create a textbox
-char textbox_GetFocus(TextBox *tb); //Check if this textbox has focus
-void textbox_BringFocus(TextBox *tb); //Make the textbox focused
-void textbox_EscapeFocus(TextBox *tb); //Make the textbox no longer have focus
-void textbox_AppendText(TextBox *tb, char text, SDL_Color color); //Appends a textbox with a character with the specified color from the current caret position.
-float textbox_GetTextWidth(TextBox *tb); //Gets the pixel width of the text within the textbox
-void textbox_SetCaretPos(TextBox *tb, int pos); //Sets the position of the caret
-void textbox_HighlightText(TextBox *tb, int from, int to); //Highlights text from the given range
-void textbox_Free(TextBox *tb);
+TextBox* mint_CreateTextBox(int x, int y, int width, int height); //Create a textbox
+char mint_TextBoxGetFocus(TextBox *tb); //Check if this textbox has focus
+void mint_TextBoxBringFocus(TextBox *tb); //Make the textbox focused
+void mint_TextBoxEscapeFocus(TextBox *tb); //Make the textbox no longer have focus
+void mint_TextBoxAppendText(TextBox *tb, char text, SDL_Color color); //Appends a textbox with a character with the specified color from the current caret position.
+float mint_TextBoxGetTextWidth(TextBox *tb); //Gets the pixel width of the text within the textbox
+void mint_TextBoxSetCaretPos(TextBox *tb, int pos); //Sets the position of the caret
+void mint_TextBoxHighlightText(TextBox *tb, int from, int to); //Highlights text from the given range
+void mint_TextBoxFree(TextBox *tb);
 
 #endif
